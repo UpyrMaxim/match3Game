2a055f33459126f21b66970206ef2ac878573297
Upyr <mupyr@mupyr-lws>
2020-01-31 11:34:18 +0200

    fix style

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <QDebug>
 #include "match3model.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
@@ -15,17 +15,17 @@ int main(int argc, char *argv[])
     qmlRegisterType<Match3Model>("custom.Match3Model", 1, 0, "Match3Model");
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
+    QObject::connect(& engine, & QQmlApplicationEngine::objectCreated,
+                     & app, [url](QObject * obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
 
     // connect
-    QObject *rootObject = engine.rootObjects().first();
-    QObject *matchModel = rootObject->findChild<QObject*>("matchModel");
-    QObject *gameBoard= rootObject->findChild<QObject*>("gameBoard");
+    QObject * rootObject = engine.rootObjects().first();
+    QObject * matchModel = rootObject->findChild<QObject *>("matchModel");
+    QObject * gameBoard = rootObject->findChild<QObject *>("gameBoard");
 
     QObject::connect(gameBoard, SIGNAL(elementAdded(int)),
                      matchModel, SLOT(OnElementAdd(int)));
